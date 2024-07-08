71ba15af1ed74ce5769ee28b775709b2e78eedae
alexsantos-dev <alexsantos.web.developer@gmail.com>
2024-07-08 17:02:36 -0300

    update db

--- database/database.config.ts
+++ database/database.config.ts
@@ -1,12 +1,21 @@
 import { TypeOrmModuleOptions } from '@nestjs/typeorm'
-import * as path from 'path'
 import { NoteEntity } from 'src/modules/note/entities/note.entity'
 import { UserEntity } from 'src/modules/user/entities/user.entity'
+import * as dotenv from 'dotenv'
 
+
+dotenv.config()
 export const dataBaseConfig: TypeOrmModuleOptions = {
-  type: 'sqlite',
-  database: 'database/database.sqlite',
+  type: 'postgres',
+  url: process.env.POSTGRES_URL,
   entities: [UserEntity, NoteEntity],
-  migrations: [path.join(__dirname, 'migrations', '*.ts')],
   synchronize: true,
 }
+
+/* 
+host: process.env.POSTGRES_HOST,
+  port: parseInt(process.env.POSTGRES_PORT),
+  username: process.env.POSTGRES_USER,
+  password: process.env.POSTGRES_PASSWORD,
+  database: process.env.POSTGRES_DATABASE,
+*/
